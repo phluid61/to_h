2994a04c984489de659ae41a2585d421e188b1b9
Kenichi Kamiya <kachick1@gmail.com>
2015-06-06 00:59:04 +0900

    Implement Array#to_h

--- lib/to_h.rb
+++ lib/to_h.rb
@@ -64,4 +64,29 @@ if RUBY_VERSION < '2.1'
     end
 
   end
+
+  class Array
+
+    # Delegating to Enumerable#to_h is OK.
+    # This override keeps arity and better warnings.
+    # @return [Hash]
+    def to_h
+      hash = {}
+
+      each_with_index do |element, index|
+        unless element.respond_to? :to_ary
+          raise TypeError, "wrong element type #{element.class} at #{index} (expected array)"
+        end
+        pair = element.to_ary
+        unless pair.length == 2
+          raise ArgumentError, "wrong array length at #{index} (expected 2, was #{pair.length})"
+        end
+        hash[pair[0]] = pair[1]
+      end
+
+      hash
+    end
+
+  end
+
 end
